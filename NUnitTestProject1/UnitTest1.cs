f6ada508a3b74eeb299f44482546cc55af4d3384
joyline.m <joyline.m@BG1NB683>
2020-08-03 18:59:38 +0530

    New code

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -13,19 +13,41 @@ namespace NUnitTestProject1
         public void Setup()
         {
             //Navigate to Google Page
-            driver.Navigate().GoToUrl("https://www.google.com/");
+            //driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Navigate().GoToUrl("http://www.durgambabooking.com/");    
 
         }
 
         [Test]
         public void Test1()
         {
-            //Find the element
+            /*//Find the element
             IWebElement element = driver.FindElement(By.Name("q"));
 
             //Perform ops
             element.SendKeys("test");
-            element.SendKeys(Keys.Enter);
+            element.SendKeys(Keys.Enter);*/
+            driver.FindElement(By.XPath(".//img[@title='click to close']")).Click();
+
+            driver.FindElement(By.Id("triptype_roundtrip")).Click();
+            driver.FindElement(By.XPath(".//a[@class='chosen-single']")).Click();
+            IWebElement FromPlace= driver.FindElement(By.XPath(".//input[@type='text']"));
+            FromPlace.SendKeys("Mysore");
+            FromPlace.SendKeys(Keys.Enter);
+
+            driver.FindElement(By.XPath("//div[@id='searchbus_to_chosen']/a/span")).Click();
+            IWebElement ToPlace = driver.FindElement(By.XPath(".//div[@id='searchbus_to_chosen']/div/div/input"));
+            ToPlace.SendKeys("Mangalore");
+            ToPlace.SendKeys(Keys.Enter);
+
+            driver.FindElement(By.Id("searchbus_depart")).Click();
+            driver.FindElement(By.XPath(".//a[contains(text(),'12')]")).Click();
+
+            driver.FindElement(By.Id("searchbus_return")).Click();
+            driver.FindElement(By.XPath(".//div[@id='ui-datepicker-div']/div/table/tbody/tr[3]/td[4]/a")).Click();
+
+            driver.FindElement(By.Name("commit")).Click();
+
         }
 
         [TearDown]
