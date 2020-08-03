4e8f6b782f7820a31288b068bbf34b4fc66d1dc6
Joyline Melita Mendonca <joyline.m@sonata-software.com>
2020-08-03 11:29:40 +0530

    Add project files.

---
+++ NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        //Create the reference for our browser
+        IWebDriver driver = new FirefoxDriver();
+
+        [SetUp]
+        public void Setup()
+        {
+            //Navigate to Google Page
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //Find the element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            //Perform ops
+            element.SendKeys("test");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+        }
+
+    }
+}
